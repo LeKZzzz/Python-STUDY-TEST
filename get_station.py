b051ae7f915854df678517a01c398eff4afdded1
LeK <1148579949@qq.com>
2022-08-17 23:29:10 +0800

    2022/8/17更新

--- get_station.py
+++ get_station.py
@@ -9,25 +9,29 @@ import requests, re, os
 def getStation():
     #   发送请求获取所有车站名称，通过输入的站名转换为查询地址的参数
     url = 'https://kyfw.12306.cn/otn/resources/js/framework/station_name.js?station_version=1.9236'
-    response = requests.get(url, verify=True)   # 请求并进行验证
+    response = requests.get(url, verify=True)  # 请求并进行验证
     stations = re.findall(u'([\u4e00-\u9fa5]+)\|([A-Z]+)', response.text)
     stations = dict(stations, indent=4)
     stations = str(stations)
     write(stations)
 
+
 def write(stations):
-    file = open('stations.text','w',encoding='utf_8_sig')
+    # 写入文件
+    file = open('stations.text', 'w', encoding='utf_8_sig')
     file.write(stations)
     file.close
 
+
 def read():
-    file = open('stations.text','r',encoding='utf_8_sig')
-    data = file.realine()
+    # 读取文件，返回文件数据
+    file = open('stations.text', 'r', encoding='utf_8_sig')
+    data = file.readline()
     file.close()
     return data
 
+
 def isStations():
+    # 判断文件是否存在
     isStations = os.path.exists('stations.text')
     return isStations
-
-
--- query_request.py
+++ query_request.py
@@ -20,18 +20,109 @@ def query(date, from_station, to_station):
     if isStations() == True:
         stations = eval(read())
         if len(result) != 0:
-           for i in result:
-               tmp_list = i.split('|')
-               from_station = list(stations.keys())[list(stations.values()).index(tmp_list[6])]
-               to_station = list(stations.keys())[list(stations.values()).index(tmp_list[7])]
-               seat = [tmp_list[3],from_station,to_station,tmp_list[8],tmp_list[9],tmp_list[10],tmp_list[32],tmp_list[31],
-                       tmp_list[30],tmp_list[21],tmp_list[23],tmp_list[33],tmp_list[28],tmp_list[24],tmp_list[29],tmp_list[26]]
-               newSeat=[]
-               for s in seat:
-                   if s == '':
-                       s='--'
-                   else:
-                       s=s
+            for i in result:
+                tmp_list = i.split('|')
+                from_station = list(stations.keys())[list(stations.values()).index(tmp_list[6])]
+                to_station = list(stations.keys())[list(stations.values()).index(tmp_list[7])]
+                seat = [tmp_list[3], from_station, to_station, tmp_list[8], tmp_list[9], tmp_list[10], tmp_list[32],
+                        tmp_list[31], tmp_list[30], tmp_list[21], tmp_list[23], tmp_list[33], tmp_list[28],
+                        tmp_list[24],
+                        tmp_list[29], tmp_list[26]]
+                newSeat = []
+                for s in seat:
+                    if s == '':
+                        s = '--'
+                    else:
+                        s = s
                     newSeat.append(s)
                 data.append(newSeat)
     return data
+
+
+# 获取高铁信息的方法
+def g_vehicle():
+    if len(data) != 0:
+        for g in data:
+            i = g[0].startswith('G')
+            if i:
+                type_data.append(g)
+
+
+# 移除高铁信息的方法
+def r_g_vehicle():
+    if len(data) != 0 and len(type_data) != 0:
+        for g in data :
+            i = g[0].startswith('G')
+            if i:
+                type_data.remove(g)
+
+# 获取动车信息的方法
+def d_vehicle():
+    if len(data) != 0:
+        for g in data:
+            i = g[0].startswith('D')
+            if i:
+                type_data.append(g)
+
+
+# 移除动车信息的方法
+def r_d_vehicle():
+    if len(data) != 0 and len(type_data) != 0:
+        for g in data :
+            i = g[0].startswith('D')
+            if i:
+                type_data.remove(g)
+
+
+# 获取直达信息的方法
+def z_vehicle():
+    if len(data) != 0:
+        for g in data:
+            i = g[0].startswith('Z')
+            if i:
+                type_data.append(g)
+
+
+# 移除直达信息的方法
+def r_z_vehicle():
+    if len(data) != 0 and len(type_data) != 0:
+        for g in data :
+            i = g[0].startswith('Z')
+            if i:
+                type_data.remove(g)
+
+
+# 获取特快信息的方法
+def t_vehicle():
+    if len(data) != 0:
+        for g in data:
+            i = g[0].startswith('T')
+            if i:
+                type_data.append(g)
+
+
+# 移除特快信息的方法
+def r_t_vehicle():
+    if len(data) != 0 and len(type_data) != 0:
+        for g in data :
+            i = g[0].startswith('T')
+            if i:
+                type_data.remove(g)
+
+
+# 获取快速信息的方法
+def k_vehicle():
+    if len(data) != 0:
+        for g in data:
+            i = g[0].startswith('K')
+            if i:
+                type_data.append(g)
+
+
+# 移除快速信息的方法
+def r_k_vehicle():
+    if len(data) != 0 and len(type_data) != 0:
+        for g in data :
+            i = g[0].startswith('K')
+            if i:
+                type_data.remove(g)
